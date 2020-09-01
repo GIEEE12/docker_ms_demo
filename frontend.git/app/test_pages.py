eb2dd0037e52e5009412b947a90a8fa3a1d24810
GIEEE12 <gieee12.arunkumar@gmail.com>
2020-09-01 06:10:33 +0000

    modified frontend test file

--- frontend.git/app/test_pages.py
+++ frontend.git/app/test_pages.py
@@ -5,17 +5,21 @@ import requests
 
 class TestFlaskApiUsingRequests(TestCase):
     def test_home(self):
+        print("checking for homepage")
         response = requests.get('http://localhost')
         self.assertEqual(response.status_code, 200)
 
     def test_login(self):
+        print('Checking for login page')
         response = requests.get('http://localhost/login')
         self.assertEqual(response.status_code, 200)
 
     def test_register(self):
+        print("checking for register page")
         response = requests.get('http://localhost/register')
         self.assertEqual(response.status_code, 200)
 
     def test_checkout(self):
+        print("checking for checkout page")
         response = requests.get('http://localhost/checkout')
         self.assertEqual(response.status_code, 200)
