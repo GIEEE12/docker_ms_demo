fd27f9aef2c15c54dee967309e7fbaca6f6d7015
GIEEE12 <gieee12.arunkumar@gmail.com>
2020-09-01 05:47:24 +0000

    added tests

--- frontend.git/tests/test_pages.py
+++ frontend.git/tests/test_pages.py
@@ -5,17 +5,17 @@ import requests
 
 class TestFlaskApiUsingRequests(TestCase):
     def test_home(self):
-        response = requests.get('http://192.168.99.100')
+        response = requests.get('http://localhost')
         self.assertEqual(response.status_code, 200)
 
     def test_login(self):
-        response = requests.get('http://192.168.99.100/login')
+        response = requests.get('http://localhost/login')
         self.assertEqual(response.status_code, 200)
 
     def test_register(self):
-        response = requests.get('http://192.168.99.100/register')
+        response = requests.get('http://localhost/register')
         self.assertEqual(response.status_code, 200)
 
     def test_checkout(self):
-        response = requests.get('http://192.168.99.100/checkout')
+        response = requests.get('http://localhost/checkout')
         self.assertEqual(response.status_code, 200)
--- order_service.git/tests/test_endpoints.py
+++ order_service.git/tests/test_endpoints.py
@@ -5,7 +5,7 @@ import requests
 
 class TestFlaskApiUsingRequests(TestCase):
     def test_products(self):
-        response = requests.get('http://192.168.99.100:8081/api/products')
+        response = requests.get('http://localhost:8081/api/products')
         self.assertEqual(response.status_code, 200)
 
     def test_create(self):
@@ -17,9 +17,9 @@ class TestFlaskApiUsingRequests(TestCase):
                 "slug": "product-1"
             }
         }
-        response = requests.post('http://192.168.99.100:8081/api/product/create', payload)
+        response = requests.post('http://localhost:8081/api/product/create', payload)
         self.assertEqual(response.status_code, 200)
 
     def test_product(self):
-        response = requests.get('http://192.168.99.100:8081/api/product/product-1')
+        response = requests.get('http://localhost:8081/api/product/product-1')
         self.assertEqual(response.status_code, 200)
